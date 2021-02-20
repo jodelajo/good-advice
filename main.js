4764bf807222720ab693c5a5b608975bb921d86b
jodelajo <jodelajo@gmail.com>
2021-02-20 20:00:20 +0100

    DOM, html, css

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 async function gimmySomeAdvice (){
     try {
-        const searchSubject = "sun";
+        const inputElement = document.getElementById('input');
+        let searchSubject = inputElement.value;
         const url = (`https://api.adviceslip.com/advice/search/${searchSubject}`);
         const response = await axios.get(url);
         console.log(response);
@@ -10,7 +11,7 @@ async function gimmySomeAdvice (){
 
     }
 }
-gimmySomeAdvice()
+
 
 function totalAdvices (slipArray){
     let myAdvice = [];
@@ -19,9 +20,20 @@ function totalAdvices (slipArray){
         console.log('Here is some good advice:', myAdvice);
         const adviceList = document.getElementById('lijst');
         const listItem = document.createElement('li');
-        listItem.textContent = `${myAdvice.advice}`;
+        listItem.setAttribute('class', 'tegel');
+        listItem.textContent = `"${myAdvice.advice}"`;
         console.log('wat is dit?', listItem);
         adviceList.appendChild(listItem);
 
     }
 }
+function adviceInput(event) {
+    if (event.code === "Enter") {
+        gimmySomeAdvice()
+    }
+}
+
+const pushbutton = document.getElementById("button");
+pushbutton.addEventListener('click', gimmySomeAdvice);
+const searchAdvice = document.getElementById("input");
+searchAdvice.addEventListener('keypress', adviceInput);
