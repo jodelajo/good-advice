396284715221e3077e4f77828b22a5ed4c5f7ad7
jodelajo <jodelajo@gmail.com>
2021-02-21 16:36:42 +0100

    laatste functionaliteiten

--- main.js
+++ main.js
@@ -1,39 +1,62 @@
 async function gimmySomeAdvice (){
+    const inputElement = document.getElementById('input');
+    let searchSubject = inputElement.value;
+    const url = (`https://api.adviceslip.com/advice/search/${searchSubject}`);
+    const response = await axios.get(url);
+    const slips = response.data.slips;
+
+    console.log(slips[0].advice)
+    console.log(response);
+    console.log(response.data)
+    // console.log(response.data.message)
+    // console.log(response.data.message.text)
+    searchAdvice.value= "";
+
+    removeChilderen(adviceContainer);
+
     try {
-        const inputElement = document.getElementById('input');
-        let searchSubject = inputElement.value;
-        const url = (`https://api.adviceslip.com/advice/search/${searchSubject}`);
-        const response = await axios.get(url);
-        console.log(response);
-        const slips = response.data.slips;
-        const listOfAdvices = totalAdvices(slips)
+        console.log('HI', response)
+        // const errorMessage = response.data.message.text;
+        // console.log(response.data.message.text)
+       totalAdvices(slips);
     } catch(e){
-
+            console.log('ERRORMESSAGE')
+        }
     }
-}
-
 
 function totalAdvices (slipArray){
     let myAdvice = [];
     for (let i = 0; i < slipArray.length; i++) {
         myAdvice = slipArray[i];
-        console.log('Here is some good advice:', myAdvice[i]);
+        console.log('Here is some good advice:', myAdvice);
         const adviceList = document.getElementById('lijst');
         const listItem = document.createElement('li');
         listItem.setAttribute('class', 'tegel');
         listItem.textContent = `"${myAdvice.advice}"`;
         console.log('wat is dit?', listItem);
         adviceList.appendChild(listItem);
-
     }
 }
+
+
+// let query = '';
 function adviceInput(event) {
+    // query = event.target.value;
+    // console.log(query)
     if (event.code === "Enter") {
         gimmySomeAdvice()
     }
 }
 
+function removeChilderen(node) {
+    while (node.firstChild) {
+        node.removeChild(node.lastChild)
+    }
+}
 const pushbutton = document.getElementById("button");
 pushbutton.addEventListener('click', gimmySomeAdvice);
 const searchAdvice = document.getElementById("input");
 searchAdvice.addEventListener('keypress', adviceInput);
+const adviceContainer = document.getElementById('lijst')
+const adviceItem = document.getElementsByClassName('tegel');
+
---
+++ notes.md
@@ -0,0 +1,6 @@
+Css:
+nice grid
+repeat(auto-fit, minmax(240px, 1fr));
+
+JAVASCRIPT
+for loop
